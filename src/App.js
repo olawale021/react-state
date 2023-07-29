72de81e1658bfd5e09e57955b2348d38daa01838
olawale021 <olawalefilani@gmail.com>
2023-07-29 12:48:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Lionel Messi',
+      bio: "Football legend, record-breaker, GOAT. Messi: Master of the pitch.",
+      imgSrc: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2023-06/Messi_Miami_FTR_2.png?h=8bbf2749&itok=hFu-Cobm',
+      profession: 'Actor',
+    },
+    show: false,
+    intervalId: null,
+    timeSinceMount: 0,
+  };
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  componentDidMount() {
+    this.setState({ intervalId: setInterval(this.updateTimeSinceMount, 1000) });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  updateTimeSinceMount = () => {
+    this.setState((prevState) => ({ timeSinceMount: prevState.timeSinceMount + 1 }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeSinceMount } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className='person'>PERSON PROFILE</h1>
+        <button onClick={this.toggleProfile}>{show ? 'Hide Profile' : 'Show Profile'}</button>
+        {show && (
+          <div className="profile">
+            <img style={{width : '35rem'}} src={imgSrc} alt="Profile" />
+            <h2 className='name'>{fullName}</h2>
+            <p className='bio'>{bio}</p>
+            <span>PROFESSION: {profession}</span>
+          </div>
+        )}
+        <p className='tsm'>Time since mount: {timeSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
